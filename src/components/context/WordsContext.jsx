138cb9d53c9ca67e2772a89f3c31809e837410d4
Abdullah Alsuhaimy <a.alsuhaimy@labayh.net>
2023-09-04 01:33:53 +0300

    Created a context to make words global

---
+++ src/components/context/WordsContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+const WordsContext = createContext()
+
+export const WordsProvider = ({children}) => {
+
+    const[words, setWords] = useState([])
+
+    return <WordsContext.Provider value={{
+        words, 
+        setWords
+    }}>
+        {children}
+    </WordsContext.Provider>
+}
+
+export default WordsContext
