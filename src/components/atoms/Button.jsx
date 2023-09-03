aa24ef0a18d88846e5618ef5803b4ec88d84e98a
Abdullah Alsuhaimy <a.alsuhaimy@labayh.net>
2023-09-04 01:32:46 +0300

    created button component

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Button({ onBtnClick, label, outline }) {
+  return (
+    <button
+      onClick={onBtnClick}
+      className={`text-2xl px-16 py-3 mx-2 rounded-3xl transition-all duration-500 ${
+        outline
+          ? "bg-white text-black hover:bg-gray-100"
+          : "text-white bg-black border-none hover:opacity-80"
+      }`}
+      style={{border: `${outline && '1px solid black'}`}}
+    >
+      {label}
+    </button>
+  );
+}
+
+export default Button;
