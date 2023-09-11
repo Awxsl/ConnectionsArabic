c8f50706398a689ccb1f49f97a157a95725ea6f2
Abdullah Alsuhaimy <a.alsuhaimy@labayh.net>
2023-09-11 05:38:17 +0300

    created component for displaying solutions to user

---
+++ src/components/molecules/SolutionGroupCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function SolutionGroupCard({ header, items, idx }) {
+  const colors = ["#f9df6d", "#a0c35a", "#b0c4ef", "#ba81c5"];
+
+  return (
+    <div
+      className={`w-full flex flex-col rounded-md items-center py-5 text-black`}
+      style={{backgroundColor: `${colors[idx]}`}}
+    >
+      <h3 className="text-4xl pb-4 font-semibold">{header}</h3>
+      <div className="flex flex-row-reverse text-2xl">
+        {items.map((item, i) => (
+          <p className="px-1">{`${
+            i !== items.length - 1 ? "،" : ""
+          } ${item}`}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SolutionGroupCard;
