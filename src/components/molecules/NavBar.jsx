1d65c9f9585e40e4c96238e90cc912a90113f4f3
Abdullah Alsuhaimy <a.alsuhaimy@labayh.net>
2023-09-12 03:20:45 +0300

    Created navbar

---
+++ src/components/molecules/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='fixed top-0 w-screen shadow-xl py-8 flex justify-center items-center px-10'>
+        <p className='text-5xl font-bold'>وَشِيجَة</p>
+    </div>
+  )
+}
+
+export default NavBar
