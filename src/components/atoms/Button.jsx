6e53c42514d460e6fc9bdad7111aa94d964b3da9
unknown <awxslu@gmail.com>
2023-09-18 03:30:43 +0300

    added type and classname as button props

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
-function Button({ onBtnClick, label, outline, disabled }) {
+function Button({
+  onBtnClick,
+  label,
+  type = "button",
+  outline,
+  disabled,
+  className,
+}) {
   return (
     <button
       disabled={disabled}
       onClick={onBtnClick}
-      className={`text-2xl px-16 py-3 mx-2 rounded-3xl transition-all duration-500 ${
+      type={type}
+      className={`text-2xl px-16 py-3 mx-2 rounded-3xl transition-all duration-500 flex justify-center items-center ${
         outline
           ? "bg-white text-black hover:bg-gray-100"
           : "text-white bg-black border-none hover:opacity-80"
-      }`}
-      style={{border: `${outline && '1px solid black'}`}}
+      } ${className}`}
+      style={{ border: `${outline && "1px solid black"}` }}
     >
       {label}
     </button>
