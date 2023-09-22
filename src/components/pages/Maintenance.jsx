50c31f3f20977453e5c9e6689b183a9fd224ee04
unknown <awxslu@gmail.com>
2023-09-23 02:29:04 +0300

    Created maintainance page

---
+++ src/components/pages/Maintenance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Maintenance() {
+  return (
+    <div className='w-full h-[70vh] flex justify-center items-center text-3xl font-bold'>هذه الصفحة غير موجودة مؤقتاً</div>
+  )
+}
+
+export default Maintenance
