3a6c5bfba25fd328b13168f2ecbe97967e5edd9d
unknown <awxslu@gmail.com>
2023-09-23 03:21:15 +0300

    Fetched latest words by default

--- src/context/WordsContext.jsx
+++ src/context/WordsContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 import { shuffle } from "../helper/arrayOperations";
 
 const WordsContext = createContext();
@@ -10,21 +10,23 @@ export const WordsProvider = ({ children }) => {
   const [submittedWords, setSubmittedWords] = useState([]);
   const [selectedWords, setSelectedWords] = useState([]);
   const [totalChallenges, setTotalChallenges] = useState(1);
-  const [challengeNumber, setChallengeNumber] = useState(0);
-
+  const [challengeNumber, setChallengeNumber] = useState(-1);
+  
+  
   useEffect(() => {
-    getData(challengeNumber);
+    getData();
   }, [challengeNumber]);
-
-  const getData = async (challengeNumber) => {
+  
+  const getData = async () => {
     setLoading(true)
     const BASE_URL = import.meta.env.VITE_BASE_URL;
-    const response = await fetch(`${BASE_URL}/api/words/${challengeNumber}`);
+    const response = await fetch(`${BASE_URL}/api/words/${challengeNumber === -1 ? 'newWord' : challengeNumber}`);
     const { data, count } = await response.json();
     let allWords = [];
     data.forEach((group) => (allWords = [...allWords, ...group.items]));
     setWords(shuffle(allWords));
     setTotalChallenges(count);
+    setChallengeNumber(prevState => prevState === -1 ? count-1 : challengeNumber)
     setWordGroups(data);
     setLoading(false)
   };
